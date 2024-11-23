74d35cf3c2b130509701bfa6e955786bf66fc9d9
divyavut <vdivyaaws@gmail.com>
2024-11-23 12:48:32 -0500

    expense infra

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+ 
+ backend "s3" {
+    bucket = "d81s-remote-state-dev"
+    key    = "expense-vpc-dev"
+    region = "us-east-1"
+    dynamodb_table = "d81s-locking-dev"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
