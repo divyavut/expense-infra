6b2fa25b7e7d644956391eb1eb8198301ec267ce
divyavut <vdivyaaws@gmail.com>
2024-11-29 09:14:12 -0500

    expense infra dev

--- 50-acm/parameter.tf
+++ 50-acm/parameter.tf
@@ -1,5 +1,5 @@
 
-resource "aws_ssm_parameter" "export_https_certificate_arn" {
+resource "aws_ssm_parameter" "https_certificate_arn" {
   name  = "/${var.project_name}/${var.environment}/https_certificate_arn"
   type  = "StringList"
   value = aws_acm_certificate.expense.arn
